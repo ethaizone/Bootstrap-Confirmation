e38419b717e812a9de2f294bd8b7bc4fa1eb3bec
Nimit Suwannagate <ethaizone@hotmail.com>
2013-06-22 23:15:27 +0700

    Add doc with finish. ver 1.0.0

---
+++ docs/assets/js/application.js
@@ -0,0 +1,14 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+!function ($) {
+
+  $(function(){
+
+    $('[data-toggle="confirmation"]').confirmation();
+    $('[data-toggle="confirmation-singleton"]').confirmation({singleton:true});
+
+  })
+
+}(window.jQuery)
